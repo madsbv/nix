c54ad8b2f24104633479fa942771d7691fb0918e
madsbv <mbvilladsen@pm.me>
2024-01-29 16:45:21 -0500

    Make emacs work

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -10,20 +10,11 @@ let
   sharedFiles = import ../shared/files.nix { inherit config pkgs; };
   additionalFiles = import ./files.nix { inherit user config pkgs; };
 
-  my-emacs-mac = pkgs.emacs29-macport.override {
-    withNativeCompilation = true;
-    withImageMagick = true;
-  };
-  # According to https://github.com/NixOS/nixpkgs/issues/267548, the with-packages version might cause problems with doom. If so, try my-emacs-mac instead.
-  my-emacs-mac-with-packages =
-    (pkgs.emacsPackagesFor my-emacs-mac).emacsWithPackages (epkgs:
-      with epkgs; [
-        pkgs.mu
-        treesit-grammars.with-all-grammars
-        vterm
-        multi-vterm
-        pdf-tools
-      ]);
+  # HACK: Manual specification of xdg config dir, should figure out how to access config.xdg.configHome. Maybe do the xdg stuff in a separate file earlier in the process, programs later? That would make sense anyway.
+  xdg_configHome = "${config.users.users.${user}.home}/.config";
+  emacsDir = "${xdg_configHome}/emacs";
+  doomDir = "${xdg_configHome}/doom";
+
   # brew "railwaycat/emacsmacport/emacs-mac", args: ["with-imagemagick", "with-native-compilation", "with-no-title-bars", "with-starter", "with-unlimited-select", "with-xwidgets"]
 in {
   imports = [ ./dock ];
@@ -43,8 +34,7 @@ in {
 
     brews = [
       # System stuff
-      # TODO: Do I have to do something to activate noclamshell?
-      # More importantly, can I do this natively with nix-darwin?
+      # Note: Noclamshell works on current system, might need to brew services start it on a new install though.
       "pirj/noclamshell/noclamshell"
       "felixkratz/formulae/borders"
       "felixkratz/formulae/svim"
@@ -69,7 +59,6 @@ in {
   };
 
   # TODO: Try to move the xdg, sessionVariables and shellAliases parts into modules/shared somewhere. This requires that we figure out exactly where to set these things in nixos.
-  # TODO: Create a '.config' directory in home-manager directory, together with a variable referencing its absolute path in nix, and put non-nix config files in there?
 
   # Enable home-manager
   home-manager = {
@@ -108,27 +97,37 @@ in {
       };
 
       programs = {
+        # Note: Trying to use `(pkgs.emacsPackagesFor my-emacs-mac).emacsWithPackages` and an override at the same time breaks things via weird nix double wrapping issues, so use extraPackages instead.
         emacs = {
-          # TODO: Switch over from homebrew
           enable = true;
-          package = my-emacs-mac-with-packages;
+          package = pkgs.emacs29-macport.override {
+            withNativeCompilation = true;
+            withImageMagick = true;
+          };
+          extraPackages = epkgs:
+            with epkgs; [
+              # Packages that pull in non-lisp stuff
+              # The mu4e epkg also pulls in the mu binary
+              mu4e
+              treesit-grammars.with-all-grammars
+              vterm
+              multi-vterm
+              pdf-tools
+            ];
         };
       } // import ../shared/home-manager.nix { inherit config pkgs lib; };
+
       # If Doom's emacs or config folder don't already exist, get them from their respective github repos defined in flake.nix.
-      # If the emacs folder doesn't exist, install doom following https://github.com/doomemacs/doomemacs/blob/master/docs/getting_started.org#install-doom-manually
+      # If the emacs folder doesn't exist, install doom
       home.activation.installDoomEmacs =
         lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          EMACS_DIR = "${config.xdg.configHome}/emacs"
-          DOOM_DIR = "${config.xdg.configHome}/doom"
-          if [ ! -d "$DOOM_DIR" ]; then
-             ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${my-doomemacs-config}/ DOOM_DIR
+          if [ ! -d "${doomDir}" ]; then
+             ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${my-doomemacs-config}/ ${doomDir}
           fi
-          if [ ! -d "$EMACS_DIR" ]; then
-             ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${doomemacs}/ $EMACS_DIR
-             PATH = "$EMACS_DIR/bin:$PATH"
-             doom sync
-             doom env
-             emacs --batch -f nerd-icons-install-fonts
+          if [ ! -d "${emacsDir}" ]; then
+             ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${doomemacs}/ ${emacsDir}
+             export PATH="${emacsDir}/bin:$PATH"
+             doom install
           fi
         '';
     };
