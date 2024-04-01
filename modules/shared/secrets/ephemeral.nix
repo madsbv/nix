f8e5405296c4111d3cf5607ba42b85519e35ca13
Mads Bach Villadsen <mvilladsen@pm.me>
2024-04-01 16:03:58 +0200

    Try to fix tailscale authkey in installer

--- modules/shared/secrets/ephemeral.nix
+++ modules/shared/secrets/ephemeral.nix
@@ -4,7 +4,10 @@
   age.secrets = {
     ssh-host-ephemeral.rekeyFile = flake-root
       + "/secrets/ssh/id_ed25519-ephemeral.age";
-    tailscale-ephemeral-vms-authkey.rekeyFile = flake-root
-      + "/secrets/tailscale/24-03-30-ephemeral-vms.authkey.age";
+    tailscale-ephemeral-vms-authkey = {
+      rekeyFile = flake-root
+        + "/secrets/tailscale/24-03-30-ephemeral-vms-authkey.age";
+      name = "tailscale-authkey";
+    };
   };
 }
