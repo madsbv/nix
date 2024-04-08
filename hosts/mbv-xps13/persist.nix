79ef92d9001e678aa0962da1c1215bf219719a40
Mads Bach Villadsen <mvilladsen@pm.me>
2024-04-08 15:00:13 +0200

    Persist restic cache

--- hosts/mbv-xps13/persist.nix
+++ hosts/mbv-xps13/persist.nix
@@ -3,13 +3,14 @@ _:
 {
   environment.persistence."/nix/persist" = {
     hideMounts = true;
-    # TODO: We might want to persist some tailscale state
     directories = [
       "/etc/nixos"
       "/etc/NetworkManager/system-connections"
       "/etc/ssh" # We need the entire directory so we can set neededForBoot
       "/var/log"
       "/var/lib"
+      # Restic basically needs its cache to be able to run in a reasonable amount of time
+      "/var/cache/restic-backups-persist"
     ];
     files = [ "/etc/machine-id" ];
   };
