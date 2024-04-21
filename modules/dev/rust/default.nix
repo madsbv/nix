19c45eebda4abf2e9d417dba4ded7e7a0a3b13ee
Mads Bach Villadsen <mvilladsen@pm.me>
2024-04-21 15:48:57 +0200

    Fix path, set up bacon config incl semver-checks

--- modules/dev/rust/default.nix
+++ modules/dev/rust/default.nix
@@ -11,8 +11,55 @@
     {
       programs.bacon = {
         enable = true;
-        settings = { };
+        settings = {
+          keybindings = {
+            # Some defaults
+            s = "toggle-summary";
+            w = "toggle-wrap";
+            b = "toggle-backtrace";
+            # Vim modifications of defaults
+            esc = "back";
+            g = "scroll-to-top";
+            shift-g = "scroll-to-bottom";
+            k = "scroll-lines(-1)";
+            j = "scroll-lines(1)";
+            ctrl-u = "scroll-page(-1)";
+            ctrl-d = "scroll-page(1)";
+            # Default job commands
+            a = "job:check-all";
+            i = "job:initial";
+            c = "job:clippy"; # Already runs clippy with --all-targets
+            d = "job:doc-open";
+            t = "job:test";
+            r = "job:run";
+            # Custom job commands
+            f = "job:clippy-fix";
+            v = "job:semver-checks";
+          };
+          jobs = {
+            semver-checks = {
+              command = [
+                "cargo"
+                "semver-checks"
+              ];
+              need_stdout = true;
+            };
+            clippy-fix = {
+              command = [
+                "cargo"
+                "clippy"
+                "--fix"
+                "--color"
+                "always"
+              ];
+              need_stdout = false;
+            };
+          };
+        };
       };
+
+      # Enables using programs installed via Cargo
+      home.sessionPath = [ "$HOME/.cargo/bin" ];
     }
   ];
 
@@ -22,7 +69,7 @@
     # To get stable or beta toolchain, do ..darwin.stable.defaultToolchain, e.g., or to get the complete toolchain (including stuff like MIRI that I probably don't need) replace default.toolchain with complete.toolchain or latest.toolchain.
     # Can also get toolchains for specified targets, e.g. targets.wasm32-unknown-unknown.latest.toolchain
     fenix.packages."${pkgs.system}".latest.toolchain
-    # rust
+    # XXX: Define a justfile type situation for running common checks as one job? Could be more bacon jobs, but some of these are not really suitable for that
     cargo-audit
     cargo-flamegraph
     cargo-generate
