c3ae1d68d5079411a78b093df1aea989e281976f
Mads Bach Villadsen <mvilladsen@pm.me>
2024-04-22 23:00:06 +0200

    Expand home-assistant config, add transmission

--- hosts/mbv-xps13/default.nix
+++ hosts/mbv-xps13/default.nix
@@ -14,21 +14,39 @@
 
   services = {
     # TODO: Configure
+    # Available on mbv-xps13:8123
     home-assistant = {
       enable = true;
       configWritable = true;
+      extraComponents = [
+        "awair"
+        "accuweather"
+        "tailscale"
+        # Supposedly for Smart Life
+        # See also https://github.com/rospogrigio/localtuya
+        "tuya"
+        "wake_on_lan"
+        "plex"
+        "seventeentrack"
+        "speedtestdotnet"
+      ];
       config.homeassistant = {
-        name = "Goltzstrasse";
+        name = "Berlin";
         latitude = 0.0;
         longitude = 0.0;
-        time_zone = "Europe/Amsterdam";
+        time_zone = "Europe/Berlin";
         unit_system = "metric";
         temperature_unit = "C";
       };
     };
 
-    # TODO: How to use?
-    # Temporarily turned off.
+    # TODO: Configure
+    # Available on mbv-xps13:32400/web
     plex.enable = true;
+
+    # Available on mbv-xps13:9091
+    transmission.enable = true;
+
+    # See https://www.reddit.com/r/NixOS/comments/12ibbl9/protonvpn_nixos_setup/ for potential protonvpn nixos integration options
   };
 }
