9fad748000606303ba4d6eb9f6796b7349a31bba
Mads Bach Villadsen <mvilladsen@pm.me>
2024-07-04 17:41:19 -0400

    Refactor nixos to allow clients and enable gpu

--- hosts/mbv-workstation/default.nix
+++ hosts/mbv-workstation/default.nix
@@ -1,18 +1,14 @@
-{ mod, hostname, ... }:
+{ mod, ... }:
 
 {
   imports = [
     # Generalizable config should be in default.nix, machine-specific stuff should be in configuration.nix and hardware-configuration.nix
     ./configuration.nix
-    ./gui.nix
     (mod "nixos/server")
-    (mod "shared/secrets/wifi.nix")
+    # (mod "nixos/client")
   ];
 
   local = {
-    server = {
-      inherit hostname;
-    };
     # This machine is already plenty fast, let's save some complication
     builder.enableRemoteBuilders = false;
   };
@@ -21,17 +17,36 @@
     rocmSupport = true;
   };
 
-  services.ollama = {
-    enable = true;
-    # Should really be "cuda", but we currently get a collision.
-    # I think it's related to the 'fixme' in: https://github.com/NixOS/nixpkgs/blob/bad6d5d22e7c6502d147f19b20bbbf759c5ee558/pkgs/tools/misc/ollama/default.nix#L27
-    # acceleration = false;
-    # The default, repeated for documentation
-    # listenAddress = "0.0.0.0:11434";
-    # host = "0.0.0.0";
-    # port = "11434";
-    models = "/var/lib/ollama/models";
-    home = "/var/lib/ollama";
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      # extest.enable = true; # Maybe useful for controllers?
+    };
+
+    tuxclocker = {
+      enable = true;
+      enableAMD = true;
+    };
+  };
+
+  services = {
+    # Provides blueman-applet and blueman-manager for managing bluetooth connections
+    blueman.enable = true;
+
+    ollama = {
+      enable = true;
+      # Should really be "cuda", but we currently get a collision.
+      # I think it's related to the 'fixme' in: https://github.com/NixOS/nixpkgs/blob/bad6d5d22e7c6502d147f19b20bbbf759c5ee558/pkgs/tools/misc/ollama/default.nix#L27
+      # acceleration = false;
+      # The default, repeated for documentation
+      # listenAddress = "0.0.0.0:11434";
+      # host = "0.0.0.0";
+      # port = "11434";
+      models = "/var/lib/ollama/models";
+      home = "/var/lib/ollama";
+    };
   };
   local.restic.exclude = [ "/var/lib/ollama/models" ];
 }
--- modules/nixos/client/default.nix
+++ modules/nixos/client/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   services = {
@@ -15,27 +15,29 @@
         luaModules = [ ];
       };
     };
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32bit = true;
+      };
+      pulse.enable = true;
+    };
   };
-  services.pipewire.enable = true;
+
   sound.enable = lib.mkForce false;
+
+  # Recommended by https://nixos.wiki/wiki/PipeWire
+  security.rtkit.enable = true;
+
   programs = {
-    # Could also try hyprland?
-    # sway.enable = true;
-    # waybar.enable = true;
-    #
     nm-applet.enable = true;
     spacefm.enable = true;
     firefox.enable = true;
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-      # extest.enable = true; # Maybe useful for controllers?
-    };
-
-    tuxclocker = {
-      enable = true;
-      enableAMD = true;
-    };
   };
+
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+    pwvucontrol
+  ];
 }
