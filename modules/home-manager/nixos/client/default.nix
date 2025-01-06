d9d2e260e8de92213b097aa52fe9691040914675
Mads Bach Villadsen <mvilladsen@pm.me>
2025-01-05 19:51:03 -0500

    Add Awesomewm config

--- modules/home-manager/nixos/client/default.nix
+++ modules/home-manager/nixos/client/default.nix
@@ -1,8 +1,11 @@
-{ mod, ... }:
+{ mod, flake-root, ... }:
 
 {
   imports = [
     (mod "home-manager/common/client")
     (mod "home-manager/nixos/common")
   ];
+  xdg.configFile = {
+    "awesome".source = flake-root + "/config/awesome";
+  };
 }
