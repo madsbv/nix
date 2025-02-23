4b3295f7d583d7e74336cb0536129e6aae788b59
Mads Bach Villadsen <mvilladsen@pm.me>
2025-02-22 20:00:26 -0500

    Remove transmission password

--- modules/services/media-server/transmission/default.nix
+++ modules/services/media-server/transmission/default.nix
@@ -18,7 +18,7 @@
         # From transmission docs (https://github.com/transmission/transmission/blob/main/docs/Editing-Configuration-Files.md):
         # The string starting with '{' indicates that this is an already-salted (whatever that means exactly) password.
         # Probably not safe to make public if it were an actual security measure, but it's layered with the whitelist.
-        rpc-password = "{2324d2649010be3adbde4f3b4aef125144cbd01d5l7l6xRl";
+        # rpc-password = "{2324d2649010be3adbde4f3b4aef125144cbd01d5l7l6xRl";
         # Require encrypted connections; can weaken to `1` "prefer encrypted" if 2 does not work.
         encryption = 2;
         # NOTE: There is a script-torrent-done setting to run a script on download completion. Could be used to automate transcoding and organization of files.
