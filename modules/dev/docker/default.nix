2a25e3bc560578c474e1eb258249840c05648038
Mads Bach Villadsen <mvilladsen@pm.me>
2025-03-23 12:06:45 -0400

    Remove docker/podman from darwin

--- modules/dev/docker/default.nix
+++ modules/dev/docker/default.nix
@@ -1,14 +1,22 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }:
 let
   user = "mvilladsen";
-
 in
 {
-  environment.systemPackages = with pkgs; [ docker ];
-  virtualisation.docker.enable = lib.mkIf pkgs.stdenv.isLinux true;
-  users.users.${user}.extraGroups = [ "docker" ];
+  # TODO: docker/podman options (in system or home-manager) do not support Darwin. How to configure?
+  # We could try to exclude import of this file from dev/, or just universally enable this on linux through system, or not enable any services and just install packages.
+  # environment.systemPackages = with pkgs; [
+  #   docker
+  #   podman
+  # ];
+  # users.users.${user}.extraGroups = [ "docker" ];
+  # virtualisation = {
+  #   docker.enable = true;
+  #   podman.enable = true;
+  # };
 }
