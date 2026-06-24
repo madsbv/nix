9e4cccd30944893c99dd921f6ee6cbb6c704c283
Mads Bach Villadsen <mvilladsen@pm.me>
2026-06-24 20:35:43 +0200

    fix: email module syntax

--- modules/homeManagerModules/email/default.nix
+++ modules/homeManagerModules/email/default.nix
@@ -24,97 +24,100 @@ in
       description = "Path to Protonmail Bridge password file. Required when email is enabled.";
     };
   };
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    (lib.mkAssert (cfg.muAddressArgs != null) ''
-      local.email.muAddressArgs must be set when email is enabled
-    '')
-    (lib.mkAssert (cfg.pmbridgePasswordFile != null) ''
-      local.email.pmbridgePasswordFile must be set when email is enabled
-    '')
-    {
-    home = {
-      sessionVariables = {
-        MUHOME = cfg.muhome;
-        MAILDIR = cfg.maildir;
-      };
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      (lib.mkAssert (cfg.muAddressArgs != null) ''
+        local.email.muAddressArgs must be set when email is enabled
+      '')
+      (lib.mkAssert (cfg.pmbridgePasswordFile != null) ''
+        local.email.pmbridgePasswordFile must be set when email is enabled
+      '')
+      {
+        home = {
+          sessionVariables = {
+            MUHOME = cfg.muhome;
+            MAILDIR = cfg.maildir;
+          };
 
-      activation.muInit = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        if [ ! -d "${cfg.maildir}" ]; then
-          mkdir -p "${cfg.maildir}"
-        fi
+          activation.muInit = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+            if [ ! -d "${cfg.maildir}" ]; then
+              mkdir -p "${cfg.maildir}"
+            fi
 
-        # NOTE: mu init clobbers any existing databases, so we have to guard against that manually.
-        if [ ! -d "${cfg.muhome}" ]; then
-          cat ${cfg.muAddressArgs} | xargs -I % sh -c '${pkgs.mu}/bin/mu init --maildir "${cfg.maildir}" %'
-        fi
-      '';
-    };
-
-    programs = {
-      mbsync.enable = true;
-      mu.enable = true;
-    };
-    services = {
-      imapnotify.enable = true;
-    };
-
-    accounts.email = {
-      # TODO: Add more/new addresses
-      maildirBasePath = cfg.maildir;
-      accounts."proton" = {
-        maildir.path = "Proton";
-        address = "mvilladsen@pm.me";
-        realName = "Mads Bach Villadsen";
-        userName = "mvilladsen@pm.me";
-        primary = true;
-        aliases = [ ];
-        folders = {
-          sent = "Sent";
-          inbox = "Inbox";
-          trash = "Trash";
-          drafts = "Drafts";
+            # NOTE: mu init clobbers any existing databases, so we have to guard against that manually.
+            if [ ! -d "${cfg.muhome}" ]; then
+              cat ${cfg.muAddressArgs} | xargs -I % sh -c '${pkgs.mu}/bin/mu init --maildir "${cfg.maildir}" %'
+            fi
+          '';
         };
-        imap = {
-          # We're using Protonmail Bridge, so everything happens locally
-          tls.enable = false;
-          # Might need to be 127.0.0.1 hardcoded
-          host = "127.0.0.1";
-          port = 1143;
+
+        programs = {
+          mbsync.enable = true;
+          mu.enable = true;
         };
-        smtp = {
-          host = "127.0.0.1";
-          port = 1025;
+        services = {
+          imapnotify.enable = true;
         };
-        passwordCommand = "cat ${cfg.pmbridgePasswordFile}";
-        # NOTE: The home manager mu module is not amenable to keeping secrets, use our own implementation
-        mu.enable = false;
-        mbsync = {
-          enable = true;
-          expunge = "both";
-          create = "both";
-          remove = "both";
-          subFolders = "Verbatim";
-          patterns = [
-            "*"
-            "!Recovered Messages"
-          ];
-          extraConfig = {
-            account = {
-              AuthMechs = "LOGIN";
+
+        accounts.email = {
+          # TODO: Add more/new addresses
+          maildirBasePath = cfg.maildir;
+          accounts."proton" = {
+            maildir.path = "Proton";
+            address = "mvilladsen@pm.me";
+            realName = "Mads Bach Villadsen";
+            userName = "mvilladsen@pm.me";
+            primary = true;
+            aliases = [ ];
+            folders = {
+              sent = "Sent";
+              inbox = "Inbox";
+              trash = "Trash";
+              drafts = "Drafts";
             };
-            channel = {
-              CopyArrivalDate = "yes";
-              SyncState = "*";
+            imap = {
+              # We're using Protonmail Bridge, so everything happens locally
+              tls.enable = false;
+              # Might need to be 127.0.0.1 hardcoded
+              host = "127.0.0.1";
+              port = 1143;
+            };
+            smtp = {
+              host = "127.0.0.1";
+              port = 1025;
+            };
+            passwordCommand = "cat ${cfg.pmbridgePasswordFile}";
+            # NOTE: The home manager mu module is not amenable to keeping secrets, use our own implementation
+            mu.enable = false;
+            mbsync = {
+              enable = true;
+              expunge = "both";
+              create = "both";
+              remove = "both";
+              subFolders = "Verbatim";
+              patterns = [
+                "*"
+                "!Recovered Messages"
+              ];
+              extraConfig = {
+                account = {
+                  AuthMechs = "LOGIN";
+                };
+                channel = {
+                  CopyArrivalDate = "yes";
+                  SyncState = "*";
+                };
+              };
+            };
+            imapnotify = {
+              enable = true;
+              boxes = [ "Inbox" ];
+              onNotify = "${pkgs.isync}/bin/mbsync proton";
+              onNotifyPost = "${pkgs.mu}/bin/mu index";
             };
           };
         };
-        imapnotify = {
-          enable = true;
-          boxes = [ "Inbox" ];
-          onNotify = "${pkgs.isync}/bin/mbsync proton";
-          onNotifyPost = "${pkgs.mu}/bin/mu index";
-        };
-      };
-    };
-  }]
-);
+      }
+    ]
+  );
+}
